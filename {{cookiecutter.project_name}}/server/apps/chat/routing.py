832c2b05fbb9d625844875be02493fe0a770f205
Nazar <odddellarobea@gmail.com>
2022-12-21 10:57:56 +0700

    Add routes

--- {{cookiecutter.project_name}}/server/apps/chat/routing.py
+++ {{cookiecutter.project_name}}/server/apps/chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 chat_websocket_urlpatterns = [
-    path("ws/chat/<event_id>/", consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/<room_id>/", consumers.ChatConsumer.as_asgi()),
 ]
--- {{cookiecutter.project_name}}/server/config/routing.py
+++ {{cookiecutter.project_name}}/server/config/routing.py
@@ -3,15 +3,17 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+from apps.chat.routing import chat_websocket_urlpatterns
 from apps.core.middleware import JWTQueryParamAuthMiddleware
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+websocket_urlpatterns = chat_websocket_urlpatterns
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
-  # 'websocket': JWTQueryParamAuthMiddleware(
-  #   URLRouter(),
-  # ),
+  'websocket': JWTQueryParamAuthMiddleware(
+    URLRouter(websocket_urlpatterns),
+  ),
 })
